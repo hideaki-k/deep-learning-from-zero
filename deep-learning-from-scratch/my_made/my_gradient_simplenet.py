79ddb85fa3ffe5f0b4638d0cf0eb852f722ab8f5
hideaki-k <h.kinoshita@kimura-lab.net>
2020-03-14 00:45:16 +0900

    add_sample

--- deep-learning-from-scratch/my_made/my_gradient_simplenet.py
+++ deep-learning-from-scratch/my_made/my_gradient_simplenet.py
@@ -7,14 +7,16 @@ from common.gradient import numerical_gradient
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.randn(2, 3)
-
+        #self.W = np.random.randn(2, 3)
+        self.W =np.array([[ 0.20639138, 0.45154866,  1.22533566],[-0.37647351,0.03638124,1.00944928]])
     def predict(self, x):
         return np.dot(x, self.W)
 
     def loss(self, x, t):
         z = self.predict(x)
+        print(f"z : {z}")
         y = softmax(z)
+        print(f"y : {y}")
         loss = cross_entropy_error(y, t)
 
         return loss
@@ -27,4 +29,5 @@ net = simpleNet()
 f = lambda w:net.loss(x, t)
 dW = numerical_gradient(f,net.W)
 
-print(dW)
+#print(f"net.W : {net.W}")
+print(f"dW : {dW}")
--- deep-learning-from-scratch/my_made/my_two_layer_net.py
+++ deep-learning-from-scratch/my_made/my_two_layer_net.py
@@ -3,3 +3,10 @@ import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 from common.functions import *
 from common.gradient import numerical_gradient
+
+class TwoLayerNet:
+
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
+        #重みの初期化
+        self.params = {}
+        self.params
